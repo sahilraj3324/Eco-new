84858638cdb66dab8fb9bfd5fb5ee966de75c75f
sahilraj3324 <rajsahil3324@gmail.com>
2025-05-12 11:58:26 +0530

    initial-commit

---
+++ Services/Frontend/Retailer2/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RetailerSignup from './Pages/Auth/SignUp/RetailerSignup';
+import RetailerLogin from './Pages/Auth/Login/RetailerLogin';
+
+import HomePage from './Pages/Retailers/Home/Homepage';
+import ProductDetails from './Pages/Retailers/ProductDetails/Productdetails';
+import Profile from './Pages/Retailers/Profile/Profile';
+import CartPage from './Pages/Retailers/Cart/CartPage';
+import OrderPage from './Pages/Retailers/Orders/OrderPage';
+import OrderSuccessPage from './Pages/Retailers/Orders/OrderSucess';
+import AllProductsPage from './Pages/Retailers/AllProduct/Allproduct';
+import RetailerOrders from './Pages/Retailers/Orders/RetailersOrder';
+import Navbar from './Pages/Retailers/Navbar';
+import Footer from './Pages/Retailers/Footer';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Router>
+      <Navbar />
+    <Routes>
+      <Route path="/retailerSignup" element={< RetailerSignup/>} />
+      <Route path="/retailerLogin" element={< RetailerLogin/>} />
+      <Route path="/" element={< HomePage/>} />
+      <Route path="/product/:id" element={<ProductDetails />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/cart" element={<CartPage />} />
+      <Route path="/order" element={<OrderPage />} />
+      <Route path="/ordersuccess" element={<OrderSuccessPage />} />
+      <Route path="/all" element={<AllProductsPage />} />
+      <Route path="/retailerOrder" element={<RetailerOrders />} />
+      <Route path="*" element={<h2>404 Not Found</h2>} />
+    </Routes>
+    <Footer />
+  </Router>
+  )
+}
+
+export default App
